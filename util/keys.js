ecd9ac01e2c7fa5da35e7a6d04adbd7f58246059
git-dru <mastercrypto.us@gmail.com>
2023-10-02 00:12:29 +0300

    add keys

--- util/keys.js
+++ util/keys.js
@@ -1,7 +1,10 @@
 const KEYS = {
   startDate: "2023-01-01T00:00:00Z",
   endDate: "2023-01-31T23:30:00.000Z",
-  meterId: "6514167223e3d1424bf82742"
+  meterId: "6514167223e3d1424bf82742",
+  OPENVOLT_URL: "https://api.openvolt.com/v1/",
+  apiKey: "test-Z9EB05N-07FMA5B-PYFEE46-X4ECYAR",
+  CARBON_INTENSITY_URL: 'https://api.carbonintensity.org.uk/'
 }
 
 module.exports = KEYS;
